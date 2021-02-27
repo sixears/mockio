1e7447eb5d06372ba244e288cb80c5bbbacfbe53
Mr. Fluffy <git@sixearsm>
2021-02-27 14:30:17 +0000

    r0.0.3.0

--- src/MockIO.hs
+++ src/MockIO.hs
@@ -4,16 +4,20 @@
 {-# LANGUAGE UnicodeSyntax     #-}
 
 module MockIO
-  ( DoMock(..), HasDoMock( doMock ), mkIO, mkIO', tests )
+  ( DoMock(..), HasDoMock( doMock ), mkIO, mkIO', mkIO'ME, mkIOME, tests )
 where
 
 -- base --------------------------------
 
-import Control.Monad  ( return )
+import Control.Monad  ( join, return )
 import Data.Function  ( ($) )
 import Data.String    ( String )
 import System.Exit    ( ExitCode )
 import System.IO      ( IO )
+
+-- monaderror-io -----------------------
+
+import MonadError  ( ѥ )
 
 -- monadio-plus ------------------------
 
@@ -23,6 +27,10 @@ import MonadIO  ( MonadIO, liftIO )
 
 import Data.MoreUnicode.Monad    ( (≫) )
 import Data.MoreUnicode.Natural  ( ℕ )
+
+-- mtl -----------------------
+
+import Control.Monad.Except  ( ExceptT, MonadError )
 
 -- tasty -------------------------------
 
@@ -59,12 +67,12 @@ import MockIO.DoMock  ( DoMock( DoMock, NoMock ), HasDoMock( doMock ) )
            value.
 -}
 mkIO' ∷ ∀ μ α .
-         MonadIO μ ⇒
-         IO α         -- ^ mock value; IO is available here so that, e.g., in
-                      -- ^ case of mock a file open, /dev/null is opened instead
-       → IO α         -- ^ the IO to perform when not mocked
-       → DoMock       -- ^ whether to mock
-       → μ α
+        MonadIO μ ⇒
+        IO α         -- ^ mock value; IO is available here so that, e.g., in
+                     -- ^ case of mock a file open, /dev/null is opened instead
+      → IO α         -- ^ the IO to perform when not mocked
+      → DoMock       -- ^ whether to mock
+      → μ α
 mkIO' _          io NoMock = liftIO io
 mkIO' mock_value _  DoMock = liftIO mock_value
 
@@ -72,6 +80,24 @@ mkIO' mock_value _  DoMock = liftIO mock_value
  -}
 mkIO ∷ ∀ μ α . MonadIO μ ⇒ α → IO α → DoMock → μ α
 mkIO mock_value io mck = mkIO' (return mock_value) io mck
+
+
+{- | mkIO', for `ExceptT` IO values. -}
+mkIO'ME ∷ ∀ μ ε α .
+          MonadError ε μ ⇒
+          ExceptT ε μ α -- ^ mock value; IO is available here so that, e.g., in
+                        -- ^ case of mock a file open, /dev/null is opened
+                        -- ^ instead
+        → ExceptT ε μ α -- ^ the IO to perform when not mocked
+        → DoMock        -- ^ whether to mock
+        → μ α
+
+mkIO'ME _          io NoMock = join (ѥ io)
+mkIO'ME mock_value _  DoMock = join (ѥ mock_value)
+
+{- | Mildly simplified `mkIO'ME`, specifically taking a non-IO mock value. -}
+mkIOME ∷ ∀ μ ε α . MonadError ε μ ⇒ α → ExceptT ε μ α → DoMock → μ α
+mkIOME mock_value io mck = mkIO'ME (return mock_value) io mck
 
 --------------------------------------------------------------------------------
 --                                   tests                                    --
